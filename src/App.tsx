10d239831e1489027d5c073a03947ded8c5562d5
Adetunji Samuel <samadet0001@gmail.com>
2024-05-28 17:25:41 +0200

    another update

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ function App() {
   ];
   return (
     <div>
-      <div className="bg-transparent md:bg-gray-300 w-full fixed px-3 py-4 h-28  flex justify-between md:px-24 ">
+      <div className="bg-transparent  md:bg-gradient-to-l md:from-neutral-500 to-[#898864] w-full fixed px-3 py-4 h-20 md:h-28  flex justify-between md:px-24 ">
         <div className="md:hidden">
           <HiBars3BottomLeft className="size-8 text-white" />
         </div>
@@ -90,13 +90,13 @@ function App() {
           </h2>
           <div className="md:flex md:space-x-8 md:w-full md:justify-center ">
             <div>
-              <button className="border-2 w-full  border-gray-800 text-sm text-white px-2 py-2">
+              <button className="border-2 w-full  border-neutral-500 text-sm text-white px-2 py-2">
                 INTERIOR DESIGN
               </button>
             </div>
 
             <div>
-              <button className="bg-slate-600 text-sm w-full  text-white px-4 py-2">
+              <button className="bg-gradient-to-l from-neutral-500 to-[#898864] text-sm w-full  text-white px-4 py-2">
                 SHOP NOW
               </button>
             </div>
@@ -104,17 +104,17 @@ function App() {
         </div>
       </div>
 
-      <div className="pt-10 bg-slate-100 mt-10 md:w-5/6 md:mx-auto md:px-4">
-        <div className="md:flex md:border-2 md:border-slate-200 md:p-4">
+      <div className="pt-10 bg-gradient-to-l from-neutral-400 to-[#898864] mt-10 md:w-5/6 md:mx-auto md:px-4">
+        <div className="md:flex md:border-2 md:border-neutral-200 md:p-4">
           <div className="md:w-2/3">
             <img src={imgHero} alt="img_hero" className="h-96 w-full " />
           </div>
-          <div className="bg-slate-500 w-full md:bg-transparent md:text-black">
+          <div className="bg-gradient-to-l from-neutral-500 to-[#a09e5a] w-full md:bg-none md:text-black">
             <div className="w-3/4 mx-auto text-center border-slate-300 border-2 p-4  -mt-4  md:border-none md:text-left">
-              <h2 className="text-black text-2xl">Your Creative Partner</h2>
-              <h2 className="text-white text-3xl -mt-3 md:text-black">
-                Who We Are
+              <h2 className="text-black text-2xl italic">
+                Your Creative Partner
               </h2>
+              <h2 className="text-white text-3xl -mt-3 ">WHO WE ARE</h2>
               <p className="text-white text-left md:text-black md:mt-5">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Laboriosam placeat eum cumque vero asperiores commodi
@@ -131,7 +131,7 @@ function App() {
                 impedit quasi, consectetur minima dicta voluptates ducimus
                 rerum! Error labore voluptatum molestiae!
               </p>
-              <button className="border-2 border-white px-4 py-2 md:border-black md:text-black text-white mt-5">
+              <button className="border-2 border-white px-4 py-2  text-white mt-5">
                 LEARN MORE
               </button>
             </div>
@@ -139,7 +139,7 @@ function App() {
         </div>
       </div>
 
-      <div className="pt-10 bg-slate-100 md:bg-slate-300 md:mt-36 md:py-10">
+      <div className="pt-10 pb-10 bg-gradient-to-l from-neutral-500 to-[#898864] md:mt-36 md:py-10">
         <div className="mx-auto w-2/3">
           <FaQuoteLeft className="size-8 mx-auto" />
           <p className="font-semibold italic mt-2 md:font-serif md:text-xl">
@@ -160,11 +160,13 @@ function App() {
               className="h-96 w-full md:-ml-10"
             />
           </div>
-          <div className="bg-slate-500 w-full md:bg-transparent md:text-black">
+          <div className="bg-gradient-to-l from-neutral-500 to-[#a09e5a] w-full md:bg-none md:text-black">
             <div className="w-3/4 mx-auto text-center border-slate-300 border-2 p-4  -mt-4  md:border-none md:text-left">
-              <h2 className="text-black text-2xl">Your Creative Partner</h2>
-              <h2 className="text-white text-3xl -mt-3 md:text-black">
-                Who We Are
+              <h2 className="text-black text-2xl italic">
+                Your Creative Partner
+              </h2>
+              <h2 className="text-white md:text-[#a09e5a] text-3xl -mt-3 ">
+                INTERIOR DESIGN
               </h2>
               <p className="text-white text-left md:text-black md:mt-5">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -217,11 +219,13 @@ function App() {
               className="h-96 w-full md:-ml-10"
             />
           </div>
-          <div className="bg-slate-500 w-full md:bg-transparent md:text-black">
+          <div className="bg-[#a09e5a] w-full md:bg-none md:text-black">
             <div className="w-3/4 mx-auto text-center border-slate-300 border-2 p-4  -mt-4  md:border-none md:text-left">
-              <h2 className="text-black text-2xl">Your Creative Partner</h2>
-              <h2 className="text-white text-3xl -mt-3 md:text-black">
-                Who We Are
+              <h2 className="text-black text-2xl italic">
+                Your Creative Partner
+              </h2>
+              <h2 className="text-white md:text-[#a09e5a] text-3xl -mt-3 ">
+                INTERIOR DESIGN
               </h2>
               <p className="text-white text-left md:text-black md:mt-5">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -234,15 +238,15 @@ function App() {
                 Molestias ducimus sunt vitae sit ullam delectus!
               </p>
 
-              <button className="border-2 border-white px-4 py-2 md:border-black md:text-black text-white mt-5">
-                GET CONSULTATION
+              <button className="bg-gradient-to-l text-white from-neutral-500 to-[#a09e5a] px-4 py-2  mt-5">
+                GO TO YOUR SHOWROOM
               </button>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="pt-10 bg-slate-100 md:bg-slate-300 md:mt-36 md:py-10">
+      <div className="pt-10 mt-10  md:bg-gradient-to-l md:from-neutral-500 md:to-[#898864] md:mt-36 md:py-10">
         <div className="mx-auto w-2/3">
           <FaQuoteLeft className="size-8 mx-auto" />
           <p className="font-semibold italic mt-2 md:font-serif md:text-xl">
@@ -259,11 +263,13 @@ function App() {
           <div className="md:w-4/5">
             <img src={imgHero} alt="img_hero" className="h-96 w-full " />
           </div>
-          <div className="bg-slate-500 w-full md:bg-transparent md:text-black">
+          <div className="bg-gradient-to-l from-neutral-500 to-[#a09e5a] w-full md:bg-none md:text-black">
             <div className="w-3/4 mx-auto text-center border-slate-300 border-2 p-4  -mt-4  md:border-none md:text-left">
-              <h2 className="text-black text-2xl">Your Creative Partner</h2>
-              <h2 className="text-white text-3xl -mt-3 md:text-black">
-                Who We Are
+              <h2 className="text-black text-2xl italic">
+                Your Creative Partner
+              </h2>
+              <h2 className="text-white md:text-[#a09e5a] text-3xl -mt-3 ">
+                INTERIOR DESIGN
               </h2>
               <p className="text-white text-left md:text-black md:mt-5">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -286,7 +292,7 @@ function App() {
 
       <div className="pt-20 bg-slate-100 pb-20 md:mt-20">
         <div className="mx-auto w-2/3 md:w-full">
-          <h2 className="text-slate-700 text-center text-2xl pb-10 md:text-6xl md:font-sans">
+          <h2 className="text-[#898864] font-bold text-center text-2xl pb-10 md:text-6xl md:font-sans">
             Testimonials
           </h2>
           <div>
@@ -344,7 +350,7 @@ function App() {
               </div>
             </div>
 
-            <button className="bg-slate-600 text-white px-3 py-2 mt-20 mb-10">
+            <button className="bg-gradient-to-l from-neutral-500 to-[#a09e5a] text-white px-3 py-2 mt-20 mb-10">
               SHOP NOW
             </button>
           </div>
